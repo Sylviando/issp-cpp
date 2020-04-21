4fb6e8aa38319c89136adc348cf001f4970a967f
Sylviando <64069231+Sylviando@users.noreply.github.com>
2020-04-21 15:20:28 +0200

    Update random.cpp

--- lista klasy cpp/random.cpp
+++ lista klasy cpp/random.cpp
@@ -1,6 +1,34 @@
+#include <iostream>
 #include <cassert>
+#include "random.h"
+#include <cmath>
+using namespace std;
 
-const long A = 16807;
-const long M = 2147483647;
+const long long A = 16807;
+const long long M = pow(2,31) - 1;
 
-assert(f1 > 0 && f1 < M);
+
+Random::Random(int f1)
+{
+	assert(f1 > 0 && f1 < M);
+	_fn = f1;
+}
+
+double Random::operator()()
+{
+	long long fn1 = (A * _fn) % M;
+	_fn = fn1;
+	double result = _fn * 1.0 / (M - 1);
+
+	return result;
+}
+
+int main()
+{
+    Random los;
+	for (int i = 0; i < 20; i++)
+	{
+		cout << los() << endl;
+	}
+	return 0;
+}
